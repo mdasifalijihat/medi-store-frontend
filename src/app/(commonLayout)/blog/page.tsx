cbd7ca22253c855fd1a600413d53772e44fc2fd3
mdasifalijihat <mdasifalijihat@gmail.com>
2026-02-04 14:17:27 +0600

    added login and signup

---
+++ src/app/(commonLayout)/blog/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Blogpage() {
+  return <div>Blog</div>;
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Button variant={"outline"}> Click </Button>
+      page
+    </div>
+  );
+}
